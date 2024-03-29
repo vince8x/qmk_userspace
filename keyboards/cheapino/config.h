e487b06b79d50542cbaab3fe193f6502d8ff9ed9
vince8x <vince.tran8x@gmail.com>
2024-03-29 21:17:12 +0700

    add scottoinvader

---
+++ keyboards/cheapino/config.h
@@ -0,0 +1,69 @@
+// Copyright 2023 Thomas Haukland (@tompi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
+//#define WS2812_TRST_US 80
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
+#define RGB_MATRIX_DEFAULT_VAL 32
+
+
+// Pick good defaults for enabling homerow modifiers
+// #define TAPPING_TERM 230
+
+
+#define WS2812_DI_PIN GP16 // The pin connected to the data pin of the LEDs
+ #define RGBLED_NUM 1                     // The number of LEDs connected
+
+
+#define MAX_DEFERRED_EXECUTORS 32
+
+
+// Unfortunately, some applications drop or misorder fast key events. This is a
+// partial fix to slow down the rate at which macros are sent.
+#define TAP_CODE_DELAY 12
+
+// Tap-hold configuration for home row mods.
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM_PER_KEY
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
+
+// Use Left Ctrl + Right Ctrl to activate the Command feature.
+#define IS_COMMAND() (get_mods() == MOD_MASK_CTRL)
+
+// Holding Shift while Caps Word is active inverts the shift state.
+#define CAPS_WORD_INVERT_ON_SHIFT
+// When idle, turn off Caps Word after 5 seconds.
+#define CAPS_WORD_IDLE_TIMEOUT 5000
+
+// When idle, turn off Select Word after 2 seconds.
+#define SELECT_WORD_TIMEOUT 2000
+
+// When idle, turn off Sentence Case after 2 seconds.
+#define SENTENCE_CASE_TIMEOUT 2000
+
+#define ACHORDION_STREAK
